6a898298922d839c2e27d31d0d0273b9110b80bd
MurtazaNeemuchwala <mneemuch914@gmail.com>
2020-12-03 10:31:13 -0500

    stuf

--- Discord Christmas Challenege/src/Day3/Day3.java
+++ Discord Christmas Challenege/src/Day3/Day3.java
@@ -1,12 +1,14 @@
+package Day3;
+
 import javax.swing.JFrame;
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import java.awt.*;
 
-class Day3 {
+public class Day3 {
     public static void main(String args[]) {
         JFrame frame = new JFrame();
-        ImageIcon icon = new ImageIcon("C:\\Users\\mneem\\Documents\\GitHub\\Misc\\Discord Christmas Challenege\\src\\snowman.jpg");
+        ImageIcon icon = new ImageIcon("snowman.jpg");
         JLabel label = new JLabel(icon);
         frame.add(label);
         frame.setDefaultCloseOperation
